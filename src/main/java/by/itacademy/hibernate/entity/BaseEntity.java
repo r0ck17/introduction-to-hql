80d7e3441996b36f1eb23c42711fad6d35e515da
Evgeniy <paut.evg@yandex.ru>
2023-10-20 11:35:44 +0300

    refactor(BaseEntity): removing unused imports

--- src/main/java/by/itacademy/hibernate/entity/BaseEntity.java
+++ src/main/java/by/itacademy/hibernate/entity/BaseEntity.java
@@ -1,11 +1,6 @@
 package by.itacademy.hibernate.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
 
 public interface BaseEntity<T extends Serializable> {
     T getId();
